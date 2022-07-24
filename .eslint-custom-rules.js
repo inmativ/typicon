242f35c42ba2ff21969ff7cd7a463ee24c2cf70e
Matvey.Ivanov@infowatch.com <Matvey.Ivanov@infowatch.com>
2022-07-24 17:29:36 +0400

    config eslint

--- .eslint-custom-rules.js
+++ .eslint-custom-rules.js
@@ -8,14 +8,17 @@ module.exports = {
     "key-spacing": ["error"],
     "lines-between-class-members": ["error", "always"],
     "max-len": ["error", { code: 120, ignoreStrings: true, ignoreTemplateLiterals: true }],
+    "newline-per-chained-call": ["error", { ignoreChainWithDepth: 1 }],
     "no-multiple-empty-lines": ["error", { max: 1, maxBOF: 0, maxEOF: 0 }],
     "no-multi-spaces": "error",
     "no-trailing-spaces": "error",
-    "no-useless-rename": "error", // чёт не работает
+    "no-useless-rename": "error",
     "no-whitespace-before-property": "error",
+    "object-shorthand": ["error", "always", { ignoreConstructors: false, avoidQuotes: true }],
     "padded-blocks": ["error", "never"],
     "prefer-destructuring": ["error"],
     semi: ["error", "always"],
+    "semi-spacing": ["error", { before: false, after: true }],
     "space-before-blocks": "error",
     "space-before-function-paren": ["error", "never"],
     "space-in-parens": ["error", "never"],
@@ -26,21 +29,7 @@ module.exports = {
     "@typescript-eslint/space-infix-ops": "error",
     "@typescript-eslint/object-curly-spacing": ["error", "always"],
 
-    "@angular-eslint/directive-selector": [
-      "error",
-      {
-        type: "attribute",
-        prefix: "app",
-        style: "camelCase",
-      },
-    ],
-    "@angular-eslint/component-selector": [
-      "error",
-      {
-        type: "element",
-        prefix: "app",
-        style: "kebab-case",
-      },
-    ],
+    "@angular-eslint/directive-selector": ["error", { type: "attribute", prefix: "app", style: "camelCase" }],
+    "@angular-eslint/component-selector": ["error", { type: "element", prefix: "app", style: "kebab-case" }],
   },
 };
